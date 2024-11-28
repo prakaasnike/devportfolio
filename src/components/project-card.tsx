c3f8e4e26c363efd45cf64ed02f5e2104efb2cbd
prakaasnike <erprakashnayak@gmail.com>
2024-11-28 23:47:32 +0545

    fixed project card

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -60,7 +60,7 @@ export function ProjectCard({
             className="pointer-events-none mx-auto h-40 w-full object-cover object-top" // needed because random black line at bottom of video
           />
         )}
-        {image && (
+        {image && !video && (
           <Image
             src={image}
             alt={title}
@@ -101,7 +101,9 @@ export function ProjectCard({
         {links && links.length > 0 && (
           <div className="flex flex-row flex-wrap items-start gap-1">
             {links?.map((link, idx) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
               <Link href={link?.href} key={idx} target="_blank">
+                {/* biome-ignore lint/suspicious/noArrayIndexKey: <explanation> */}
                 <Badge key={idx} className="flex gap-2 px-2 py-1 text-[10px]">
                   {link.icon}
                   {link.type}
